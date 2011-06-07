c8cb69267f7a495e488f11b084818ce6e2d8ccf6
Niko Felger <niko.felger@gmail.com>
2011-06-07 17:18:44 +0100

    Integrated DanL's gist

---
+++ lib/suenker.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'fsevents'
+require 'fileutils'
+require 'term/ansicolor'
+
+include Term::ANSIColor
+
+STDOUT.sync = true
+
+options = {
+ :directory_to_watch => "/Users/nfelger/dev/songkick",
+ :remote_host        => 'of1-dev-nfelger',
+ :remote_dir         => "dev/songkick"
+}
+
+
+def sync(options)
+  print "\033[14D", red, bold, "sünking...    ", reset
+  start = Time.now
+  cmd = "rsync -varx --delete -e ssh --filter='. #{options[:directory_to_watch]}/rsync.filter' #{options[:directory_to_watch]}/ #{options[:remote_host]}:#{options[:remote_dir]}/"
+  system(cmd)
+  $0 = "Last sünked #{Time.now.strftime("%X")}"
+  print "\033[14D"; print green, bold, "ready ", reset, "(#{(Time.now - start).to_i.to_s.rjust(5, " ")}s)", reset
+end
+
+print "              "
+
+sync(options)
+
+excluded_dirs = [".redcar", ".git", ".idea"]
+
+FileUtils.cd(options[:directory_to_watch]) do
+ stream = FSEvents::Stream.watch(options[:directory_to_watch], :latency => 1.0) do |events|
+   events.modified_files.detect do |path|
+     unless excluded_dirs.any?{ |dir| path.include?(dir) }
+       sync(options)
+       true
+     end
+   end
+ end
+ stream.run
+end
